6c47ffbc40205091429bca26f72cefe58aa5c1a4
Hurricane Hamilton <hhamilto@mtu.edu>
2016-10-08 12:56:44 -0500

    Working version 1

---
+++ public/js/main.js
@@ -0,0 +1,78 @@
+
+// better than 500 lines of polyfill to get the actual fetch that is true to WHATWG spec
+var fetchJsonResource = function(options){
+	if(typeof options == 'string') 
+		options = {url:options}
+	options = _.defaults({
+		method: 'GET'
+	},options)
+	console.log(options)
+	var request = new XMLHttpRequest()
+	var requestPromise = new Promise(function(resolve,reject){
+		request.addEventListener("load", function(){
+			resolve(this)
+		})
+	}).then(function(resp){
+		return JSON.parse(resp.responseText)
+	})
+	console.log(request)
+	request.open(options.method, options.url)
+	if(options.headers)
+		_.forEach(options.headers,_.spread(request.setRequestHeader))
+	request.send(options.body)
+	return requestPromise
+}
+
+!function(){
+	var disableInput = function(){
+		//XXX code me, if you desire
+		// If it takes a long time to submit a rating and select a new tweet
+		// to display and display it, then we should block the user from 
+		// accidentally submitting a rating
+	}
+	var enableInput = function(){
+		//XXX code me, if you desire
+	}
+	var fetchTweetIdForRating = function(){
+		// don't be fooled by fancy url. This returns an array consisting of a single tweet that needs review
+		return fetchJsonResource('api/tweets?needs_rating=true&count=1')
+		.then(function(tweets){
+			return tweets[0].id_str
+		})
+	}
+	var populateViewerWithTweet = function(tweetId){
+		var tweetContainerEl = document.getElementById('tweet-container')
+		tweetContainerEl.innerHTML = ''
+
+		return twttr.widgets.createTweet(tweetId, tweetContainerEl)
+		.then(function(val){
+			tweetContainerEl.dataset.displayedTweetId = tweetId
+			window.scrollTo(0,document.body.scrollHeight)
+			return val //not sure what val is...
+		})
+	}
+	document.addEventListener('DOMContentLoaded', function(){
+		fetchTweetIdForRating().then(populateViewerWithTweet)
+	})
+
+	// I read a blog post once about not using [].forEach.call, which really made me want to use it more.
+	![].forEach.call(document.querySelectorAll('#scale-input button'), function(element){
+		element.addEventListener('click', function(e){
+			e.preventDefault()
+			if( !e.target.dataset.rating){
+				alert('Unable to determine rating. This is broken. File an issue, then go outside and take a break from the computer.')
+				return
+			}
+			disableInput()
+			fetchJsonResource({
+				method: "PUT",
+				url: '/api/tweets/'+document.getElementById('tweet-container').dataset.displayedTweetId+'/ratings',
+				body: JSON.stringify({rating:e.target.dataset.rating}),
+				headers: [['Content-Type','application/json']]
+			})
+			.then(fetchTweetIdForRating)
+			.then(populateViewerWithTweet)
+			.then(enableInput)
+		})
+	})
+}()
---
+++ server.js
@@ -0,0 +1,37 @@
+const _ = require('lodash/fp')
+const koa = require('koa')
+const router = require('koa-route')
+const bodyParser = require('koa-bodyparser')
+const Promise = require('bluebird')
+const fs = Promise.promisifyAll(require('fs'))
+
+const dataFileNames = fs.readdirSync('tweet_data').sort()
+var tweetData; fs.readFileAsync('tweet_data/'+dataFileNames[dataFileNames.length-1])
+.then(contents=>(console.log('Last tweet data file read'), contents))
+.then(contents=> contents.toString().split('\n').slice(0,-1))
+.then(_.map(JSON.parse))
+.then(_.map(_.defaults({totalRatings:0, averageRating: 0})))
+.then(_.sortBy('totalRatings'))
+.then(val=>tweetData=val) //hacky
+
+const app = koa()
+app.use(bodyParser())
+app.use(require('koa-static')(__dirname+'/public'))
+app.use(router.get('/api/lol', function *(){
+	this.body = "it works."
+}))
+
+app.use(router.get('/api/tweets', function *(){
+	//actually just returns a single tweet
+	this.body = JSON.stringify([tweetData[0]])
+}))
+
+app.use(router.put('/api/tweets/:tweetId/ratings', function *(id_str){
+	const tweet = _.find({id_str}, tweetData)
+	tweet.totalRatings++
+	tweet.averageRating = tweet.averageRating * (tweet.totalRatings-1/tweet.totalRatings) + this.request.body.rating * (1/tweet.totalRatings)
+	tweetData = _.sortBy('totalRatings', tweetData)
+	this.body = '{"success":true}'
+}))
+
+app.listen(8080)
